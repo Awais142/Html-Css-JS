aed620fbad57d64e5de5c1975cf10f15d4d94793
Awais142 <awaisakbar142@gmail.com>
2023-08-02 11:40:00 +0500

    basic arithmatic operations

--- Calculator/script.js
+++ Calculator/script.js
@@ -6,3 +6,18 @@ buttons.forEach((button) => {
     display.textContent += button.textContent;
   });
 });
+let currentInput = "";
+let currentOperator = null;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.textContent;
+
+    if (isNaN(value) && value !== ".") {
+      currentOperator = value;
+    } else {
+      currentInput += value;
+      display.textContent = currentInput;
+    }
+  });
+});
