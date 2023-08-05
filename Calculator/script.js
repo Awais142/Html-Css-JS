b19d33208e695cfba129c8f859a5293d9bcbb160
Awais142 <awaisakbar142@gmail.com>
2023-08-05 10:40:00 +0500

    fixing erro related to equal

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,73 +1,114 @@
+// script.js
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll(".btn");
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    display.textContent += button.textContent;
-  });
-});
 let currentInput = "";
 let currentOperator = null;
+let previousInput = "";
+let resultDisplayed = false;
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const value = button.textContent;
 
-    if (isNaN(value) && value !== ".") {
-      currentOperator = value;
-    } else {
-      currentInput += value;
-      display.textContent = currentInput;
-    }
-  });
-});
-document.querySelector(".clear").addEventListener("click", () => {
-  currentInput = "";
-  display.textContent = "0";
-});
-document
-  .querySelector(".btn[content='decimal']")
-  .addEventListener("click", () => {
-    if (!currentInput.includes(".")) {
-      currentInput += ".";
-      display.textContent = currentInput;
-    }
-  });
-window.addEventListener("keydown", (e) => {
-  const key = e.key;
-  if (!isNaN(key) || key === ".") {
-    currentInput += key;
-  } else if (["+", "-", "*", "/"].includes(key)) {
-    currentOperator = key;
-  } else if (key === "Enter") {
-    display.textContent = eval(currentInput);
-    currentInput = display.textContent;
-  } else if (key === "Escape") {
-    currentInput = "";
-    display.textContent = "0";
-  }
-});
-function formatResult(value) {
-  return parseFloat(value).toFixed(2);
-}
-if (currentOperator === "/" && parseFloat(currentInput) === 0) {
-  display.textContent = "Error";
-}
-document.querySelector(".delete").addEventListener("click", () => {
-  currentInput = currentInput.slice(0, -1);
-  display.textContent = currentInput || "0";
-});
-// script.js
-document.querySelector(".btn.operator").addEventListener("click", () => {
-  if (currentOperator && currentInput) {
-    try {
-      const result = eval(currentInput); // Use of `eval` for simplicity here; better to use a safer calculation approach for production
-      display.textContent = formatResult(result);
-      currentInput = result.toString();
-      currentOperator = null;
-    } catch (error) {
-      display.textContent = "Error";
+    // Handle clear button
+    if (button.classList.contains("clear")) {
       currentInput = "";
+      previousInput = "";
+      currentOperator = null;
+      display.textContent = "0";
+      resultDisplayed = false;
+      return;
     }
-  }
+
+    // Handle delete button
+    if (button.classList.contains("delete")) {
+      if (currentInput.length > 0) {
+        currentInput = currentInput.slice(0, -1);
+      }
+      display.textContent = currentInput || "0";
+      return;
+    }
+
+    // Handle operator buttons
+    if (button.classList.contains("operator") && value !== "=") {
+      if (currentInput) {
+        if (previousInput && currentOperator) {
+          previousInput = calculate(
+            previousInput,
+            currentInput,
+            currentOperator
+          );
+          display.textContent = previousInput;
+        } else {
+          previousInput = currentInput;
+        }
+        currentInput = "";
+      }
+      currentOperator = value;
+      resultDisplayed = false;
+      return;
+    }
+
+    // Handle equals button
+    if (button.classList.contains("equals")) {
+      if (currentOperator && previousInput && currentInput) {
+        display.textContent = calculate(
+          previousInput,
+          currentInput,
+          currentOperator
+        );
+        previousInput = display.textContent;
+        currentInput = "";
+        currentOperator = null;
+        resultDisplayed = true;
+      }
+      return;
+    }
+
+    // Handle decimal input
+    if (value === ".") {
+      if (!currentInput.includes(".")) {
+        currentInput += value;
+      }
+    } else if (!isNaN(value)) {
+      // Handle number input
+      if (resultDisplayed) {
+        currentInput = value; // Start fresh after result
+        resultDisplayed = false;
+      } else {
+        currentInput += value;
+      }
+    }
+
+    display.textContent = currentInput || previousInput || "0";
+  });
 });
+
+// Helper function for calculations
+function calculate(prev, curr, operator) {
+  let result;
+  const prevNum = parseFloat(prev);
+  const currNum = parseFloat(curr);
+
+  if (isNaN(prevNum) || isNaN(currNum)) return "Error";
+
+  switch (operator) {
+    case "+":
+      result = prevNum + currNum;
+      break;
+    case "-":
+      result = prevNum - currNum;
+      break;
+    case "*":
+      result = prevNum * currNum;
+      break;
+    case "/":
+      result = currNum !== 0 ? prevNum / currNum : "Error";
+      break;
+    default:
+      return "Error";
+  }
+
+  return parseFloat(result.toFixed(10)).toString(); // Limit decimals to avoid floating point errors
+}
