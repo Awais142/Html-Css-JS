c660d80f972fb19334038a9bc87b559d2d9743d6
Awais142 <awaisakbar142@gmail.com>
2023-09-02 11:00:00 +0500

    quotes data added

---
+++ Random Quote Generator/quotes.js
@@ -0,0 +1,73 @@
+const quotes = [
+  {
+    text: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  },
+  { text: "Get busy living or get busy dying.", author: "Stephen King" },
+  {
+    text: "You only live once, but if you do it right, once is enough.",
+    author: "Mae West",
+  },
+  {
+    text: "In the end, it's not the years in your life that count. It's the life in your years.",
+    author: "Abraham Lincoln",
+  },
+  {
+    text: "Never let the fear of striking out keep you from playing the game.",
+    author: "Babe Ruth",
+  },
+  {
+    text: "Money and success don’t change people; they merely amplify what is already there.",
+    author: "Will Smith",
+  },
+  {
+    text: "Your time is limited, so don’t waste it living someone else’s life.",
+    author: "Steve Jobs",
+  },
+  {
+    text: "Not how long, but how well you have lived is the main thing.",
+    author: "Seneca",
+  },
+  {
+    text: "If life were predictable it would cease to be life, and be without flavor.",
+    author: "Eleanor Roosevelt",
+  },
+  { text: "The purpose of our lives is to be happy.", author: "Dalai Lama" },
+  {
+    text: "Life is what we make it, always has been, always will be.",
+    author: "Grandma Moses",
+  },
+  {
+    text: "Life is really simple, but we insist on making it complicated.",
+    author: "Confucius",
+  },
+  { text: "May you live all the days of your life.", author: "Jonathan Swift" },
+  {
+    text: "Life itself is the most wonderful fairy tale.",
+    author: "Hans Christian Andersen",
+  },
+  {
+    text: "Do not take life too seriously. You will never get out of it alive.",
+    author: "Elbert Hubbard",
+  },
+  {
+    text: "The best way to predict your future is to create it.",
+    author: "Abraham Lincoln",
+  },
+  {
+    text: "You have within you right now, everything you need to deal with whatever the world can throw at you.",
+    author: "Brian Tracy",
+  },
+  {
+    text: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    text: "The only impossible journey is the one you never begin.",
+    author: "Tony Robbins",
+  },
+  {
+    text: "Life is short, and it's up to you to make it sweet.",
+    author: "Sarah Louise Delany",
+  },
+];
