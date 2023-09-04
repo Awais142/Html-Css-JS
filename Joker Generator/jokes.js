d00fe77a07367583c28614dae171ce458f73e17f
Awais142 <awaisakbar142@gmail.com>
2023-09-04 11:30:00 +0500

    Joke data added

---
+++ Joker Generator/jokes.js
@@ -0,0 +1,46 @@
+const jokes = [
+  {
+    text: "Why don't scientists trust atoms? Because they make up everything! 😂",
+  },
+  {
+    text: "I told my wife she was drawing her eyebrows too high. She looked surprised! 😯",
+  },
+  {
+    text: "Why did the scarecrow win an award? Because he was outstanding in his field! 🌾",
+  },
+  {
+    text: "I told my computer I needed a break, and now it won’t stop sending me KitKat ads! 🍫",
+  },
+  { text: "How does a penguin build its house? Igloos it together! 🐧" },
+  { text: "What did the ocean say to the beach? Nothing, it just waved. 🌊" },
+  {
+    text: "I would tell you a construction pun, but I'm still working on it. 👷‍♂️",
+  },
+  {
+    text: "Why can't your nose be 12 inches long? Because then it would be a foot! 👃➡️👣",
+  },
+  { text: "Why did the bicycle fall over? Because it was two-tired! 🚲" },
+  {
+    text: "I’m reading a book on anti-gravity. It’s impossible to put down! 📘",
+  },
+  {
+    text: "Want to hear a joke about a piece of paper? Never mind, it’s tearable. 📄",
+  },
+  { text: "I used to play piano by ear, but now I use my hands. 🎹" },
+  { text: "Why do cows have hooves instead of feet? Because they lactose. 🐄" },
+  {
+    text: "What did the grape do when it got stepped on? Nothing but let out a little wine! 🍇🍷",
+  },
+  {
+    text: "I told my wife she was overreacting, but she just cried even more! 😭",
+  },
+  {
+    text: "Why did the math book look sad? Because it had too many problems. 📐",
+  },
+  {
+    text: "Parallel lines have so much in common. It’s a shame they’ll never meet. ➖➖",
+  },
+  { text: "What’s orange and sounds like a parrot? A carrot! 🥕" },
+  { text: "I made a pun about the wind but it blows. 🌬️" },
+  { text: "Need an ark to save two of every animal? I Noah guy. 🌈🐫" },
+];
