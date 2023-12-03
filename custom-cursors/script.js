d24a066a486d823512f0cdf52a2276d3ea7fcc25
Awais142 <awaisakbar142@gmail.com>
2023-12-03 10:30:00 +0500

    updated cursors

--- custom-cursors/script.js
+++ custom-cursors/script.js
@@ -1,193 +1,167 @@
-// Create cursor elements
-const cursorDot = document.createElement('div');
-const cursorOutline = document.createElement('div');
-cursorDot.className = 'cursor-dot';
-cursorOutline.className = 'cursor-outline';
-document.body.appendChild(cursorDot);
-document.body.appendChild(cursorOutline);
+// Get cursor elements
+const cursor = document.querySelector('.custom-cursor');
+const cursorOuter = document.querySelector('.custom-cursor-outer');
 
-// Track mouse movement
-let cursorVisible = true;
-let cursorEnlarged = false;
+// Initialize cursor position off screen
+let cursorX = -100;
+let cursorY = -100;
 
-const endX = window.innerWidth / 2;
-const endY = window.innerHeight / 2;
+let cursorOuterX = -100;
+let cursorOuterY = -100;
 
-let mouseX = endX;
-let mouseY = endY;
+// Update cursor position
+document.addEventListener('mousemove', (e) => {
+    cursorX = e.clientX;
+    cursorY = e.clientY;
+});
 
-function updateCursor(e) {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-
-    if (cursorVisible) {
-        cursorDot.style.transform = `translate(${mouseX}px, ${mouseY}px)`;
-        cursorOutline.style.transform = `translate(${mouseX - 16}px, ${mouseY - 16}px)`;
-    }
+// Animate cursor movement
+function animateCursor() {
+    // Smooth movement for outer cursor
+    const deltaOuterX = cursorX - cursorOuterX;
+    const deltaOuterY = cursorY - cursorOuterY;
+    
+    cursorOuterX += deltaOuterX * 0.2;
+    cursorOuterY += deltaOuterY * 0.2;
+    
+    // Update cursor positions
+    cursor.style.transform = `translate(${cursorX}px, ${cursorY}px)`;
+    cursorOuter.style.transform = `translate(${cursorOuterX}px, ${cursorOuterY}px)`;
+    
+    requestAnimationFrame(animateCursor);
 }
+
+// Start cursor animation
+animateCursor();
 
 // Handle cursor visibility
-function toggleCursorVisibility() {
-    if (cursorVisible === true) {
-        cursorDot.style.opacity = 1;
-        cursorOutline.style.opacity = 1;
-    } else {
-        cursorDot.style.opacity = 0;
-        cursorOutline.style.opacity = 0;
-    }
-}
+document.addEventListener('mouseenter', () => {
+    cursor.style.opacity = 1;
+    cursorOuter.style.opacity = 1;
+});
 
-function mouseMoveEvent(e) {
-    cursorVisible = true;
-    toggleCursorVisibility();
-    updateCursor(e);
-}
-
-function mouseLeaveEvent() {
-    cursorVisible = false;
-    toggleCursorVisibility();
-}
-
-function mouseEnterEvent() {
-    cursorVisible = true;
-    toggleCursorVisibility();
-}
+document.addEventListener('mouseleave', () => {
+    cursor.style.opacity = 0;
+    cursorOuter.style.opacity = 0;
+});
 
 // Magnetic button effect
-const magneticButtons = document.querySelectorAll('.magnetic-button');
-
-magneticButtons.forEach(button => {
+document.querySelectorAll('.magnetic').forEach(button => {
     button.addEventListener('mousemove', (e) => {
         const rect = button.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
+        const x = e.clientX - rect.left - rect.width / 2;
+        const y = e.clientY - rect.top - rect.height / 2;
         
-        button.style.transform = `translate(${x * 0.1}px, ${y * 0.1}px)`;
+        button.style.transform = `translate(${x * 0.3}px, ${y * 0.3}px)`;
+        cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px) scale(1.5)`;
+        cursorOuter.style.transform = `translate(${cursorOuterX}px, ${cursorOuterY}px) scale(1.5)`;
     });
 
     button.addEventListener('mouseleave', () => {
-        button.style.transform = 'translate(0px, 0px)';
+        button.style.transform = '';
+        cursor.style.transform = `translate(${cursorX}px, ${cursorY}px)`;
+        cursorOuter.style.transform = `translate(${cursorOuterX}px, ${cursorOuterY}px)`;
     });
 });
 
-// Drawing functionality
-const drawingAreas = document.querySelectorAll('.drawing-area');
-
-drawingAreas.forEach(area => {
-    let isDrawing = false;
-    const context = area.getContext('2d');
-    
-    area.addEventListener('mousedown', startDrawing);
-    area.addEventListener('mousemove', draw);
-    area.addEventListener('mouseup', stopDrawing);
-    area.addEventListener('mouseleave', stopDrawing);
-
-    function startDrawing(e) {
-        isDrawing = true;
-        draw(e);
-    }
-
-    function draw(e) {
-        if (!isDrawing) return;
-        const rect = area.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-
-        context.lineWidth = 2;
-        context.lineCap = 'round';
-        context.lineTo(x, y);
-        context.stroke();
-        context.beginPath();
-        context.moveTo(x, y);
-    }
-
-    function stopDrawing() {
-        isDrawing = false;
-        context.beginPath();
-    }
-});
-
-// Draggable elements
-const draggableElements = document.querySelectorAll('.draggable-element');
-
-draggableElements.forEach(element => {
-    let isDragging = false;
-    let currentX;
-    let currentY;
-    let initialX;
-    let initialY;
-    let xOffset = 0;
-    let yOffset = 0;
-
-    element.addEventListener('mousedown', dragStart);
-    element.addEventListener('mousemove', drag);
-    element.addEventListener('mouseup', dragEnd);
-    element.addEventListener('mouseleave', dragEnd);
-
-    function dragStart(e) {
-        initialX = e.clientX - xOffset;
-        initialY = e.clientY - yOffset;
-
-        if (e.target === element) {
-            isDragging = true;
-        }
-    }
-
-    function drag(e) {
-        if (isDragging) {
-            e.preventDefault();
-            currentX = e.clientX - initialX;
-            currentY = e.clientY - initialY;
-            xOffset = currentX;
-            yOffset = currentY;
-
-            element.style.transform = `translate(${currentX}px, ${currentY}px)`;
-        }
-    }
-
-    function dragEnd() {
-        initialX = currentX;
-        initialY = currentY;
-        isDragging = false;
-    }
-});
-
-// Icon cursors
-const iconCursors = document.querySelectorAll('.icon-cursor');
-
-iconCursors.forEach(element => {
-    element.addEventListener('mouseenter', () => {
-        const icon = element.getAttribute('data-icon');
-        cursorDot.setAttribute('data-icon', icon);
+// Text follow effect
+document.querySelectorAll('.text-follow').forEach(element => {
+    element.addEventListener('mousemove', (e) => {
+        const text = element.querySelector('p');
+        const rect = element.getBoundingClientRect();
+        const x = e.clientX - rect.left - rect.width / 2;
+        const y = e.clientY - rect.top - rect.height / 2;
+        
+        text.style.transform = `translate(${x * 0.1}px, ${y * 0.1}px)`;
     });
 
     element.addEventListener('mouseleave', () => {
-        cursorDot.removeAttribute('data-icon');
+        const text = element.querySelector('p');
+        text.style.transform = '';
     });
 });
 
-// Event listeners
-document.addEventListener('mousemove', mouseMoveEvent);
-document.addEventListener('mouseleave', mouseLeaveEvent);
-document.addEventListener('mouseenter', mouseEnterEvent);
+// Cursor Elements
+const cursorDot = document.querySelector('.cursor-dot');
+const cursorOutline = document.querySelector('.cursor-outline');
+const cursorButtons = document.querySelectorAll('.cursor-btn');
+const currentCursorText = document.getElementById('current-cursor');
 
-// Handle window blur
-document.addEventListener('blur', () => {
-    cursorVisible = false;
-    toggleCursorVisibility();
-});
+// Initial cursor type
+document.body.setAttribute('data-cursor', 'default');
 
-document.addEventListener('focus', () => {
-    cursorVisible = true;
-    toggleCursorVisibility();
-});
+// Mouse movement handler
+function onMouseMove(e) {
+    const posX = e.clientX;
+    const posY = e.clientY;
 
-// Initialize canvas contexts after DOM is loaded
-document.addEventListener('DOMContentLoaded', () => {
-    drawingAreas.forEach(area => {
-        const context = area.getContext('2d');
-        context.strokeStyle = '#2196f3';
-        context.lineWidth = 2;
-        context.lineCap = 'round';
+    // Animate cursor dot
+    cursorDot.style.transform = `translate(${posX}px, ${posY}px)`;
+    
+    // Add slight delay to cursor outline for smooth effect
+    requestAnimationFrame(() => {
+        cursorOutline.style.transform = `translate(${posX}px, ${posY}px)`;
     });
+}
+
+// Mouse click animation
+function onMouseDown() {
+    cursorDot.style.transform += ' scale(0.5)';
+    cursorOutline.style.transform += ' scale(0.75)';
+}
+
+function onMouseUp() {
+    cursorDot.style.transform = cursorDot.style.transform.replace(' scale(0.5)', '');
+    cursorOutline.style.transform = cursorOutline.style.transform.replace(' scale(0.75)', '');
+}
+
+// Cursor type switching
+function switchCursorType(type) {
+    document.body.setAttribute('data-cursor', type);
+    currentCursorText.textContent = type.charAt(0).toUpperCase() + type.slice(1);
+    
+    // Update active button
+    cursorButtons.forEach(btn => {
+        btn.classList.toggle('active', btn.dataset.cursor === type);
+    });
+}
+
+// Event listeners for cursor buttons
+cursorButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const cursorType = btn.dataset.cursor;
+        switchCursorType(cursorType);
+    });
+});
+
+// Add event listeners
+document.addEventListener('mousemove', onMouseMove);
+document.addEventListener('mousedown', onMouseDown);
+document.addEventListener('mouseup', onMouseUp);
+
+// Handle cursor entering/leaving elements
+const handleElementHover = (element) => {
+    element.addEventListener('mouseenter', () => {
+        cursorDot.style.transform += ' scale(1.5)';
+        cursorOutline.style.transform += ' scale(1.5)';
+    });
+
+    element.addEventListener('mouseleave', () => {
+        cursorDot.style.transform = cursorDot.style.transform.replace(' scale(1.5)', '');
+        cursorOutline.style.transform = cursorOutline.style.transform.replace(' scale(1.5)', '');
+    });
+};
+
+// Apply hover effects to interactive elements
+document.querySelectorAll('.test-btn, .test-link, .test-box').forEach(handleElementHover);
+
+// Hide default cursor when leaving the window
+document.addEventListener('mouseleave', () => {
+    cursorDot.style.opacity = '0';
+    cursorOutline.style.opacity = '0';
+});
+
+document.addEventListener('mouseenter', () => {
+    cursorDot.style.opacity = '1';
+    cursorOutline.style.opacity = '1';
 });
