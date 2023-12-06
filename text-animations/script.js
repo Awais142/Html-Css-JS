ac4c45eea80806acf6360097e46e3261bcd755cd
Awais142 <awaisakbar142@gmail.com>
2023-12-06 10:30:00 +0500

    updated

--- text-animations/script.js
+++ text-animations/script.js
@@ -5,6 +5,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const textContainers = document.querySelectorAll('.text-container');
     const customText = document.getElementById('custom-text');
     const animateBtn = document.getElementById('animate-btn');
+    const themeBtn = document.getElementById('theme-btn');
+    const speedSlider = document.getElementById('speed-slider');
+    const speedValue = document.getElementById('speed-value');
+    const fontSelector = document.getElementById('font-selector');
+    const colorPicker = document.getElementById('color-picker');
+    const presetBtns = document.querySelectorAll('.preset-btn');
+
+    // Theme toggle
+    themeBtn.addEventListener('click', () => {
+        document.body.setAttribute('data-theme',
+            document.body.getAttribute('data-theme') === 'light' ? 'dark' : 'light'
+        );
+        themeBtn.querySelector('i').classList.toggle('fa-moon');
+        themeBtn.querySelector('i').classList.toggle('fa-sun');
+    });
+
+    // Animation speed control
+    speedSlider.addEventListener('input', (e) => {
+        const speed = e.target.value;
+        speedValue.textContent = `${speed}x`;
+        document.documentElement.style.setProperty('--animation-speed', `${1/speed}s`);
+    });
 
     // Function to split text into spans with delay indices
     function splitText(element) {
@@ -21,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Function to apply animation
     function applyAnimation(element, animationType) {
         // Remove all animation classes
-        element.classList.remove('typing', 'wave', 'bounce', 'fade', 'glitch');
+        element.classList.remove('typing', 'wave', 'bounce', 'fade', 'glitch', 'rainbow', 'shake', 'neon');
         
         // Reset the text if it was split
         const originalText = element.getAttribute('data-original-text') || element.textContent;
@@ -35,12 +57,19 @@ document.addEventListener('DOMContentLoaded', () => {
             case 'wave':
             case 'bounce':
             case 'fade':
+            case 'rainbow':
                 splitText(element);
                 element.classList.add(animationType);
                 break;
             case 'glitch':
                 element.setAttribute('data-text', originalText);
                 element.classList.add('glitch');
+                break;
+            case 'shake':
+                element.classList.add('shake');
+                break;
+            case 'neon':
+                element.classList.add('neon');
                 break;
         }
     }
@@ -79,40 +108,56 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    // Function to create animated text element
+    function createAnimatedText(text) {
+        const newText = document.createElement('div');
+        newText.className = 'text-container';
+        newText.textContent = text;
+        newText.setAttribute('data-original-text', text);
+        
+        // Apply custom styles
+        newText.style.fontFamily = fontSelector.value;
+        newText.style.color = colorPicker.value;
+        
+        // Add to interactive text section
+        document.querySelector('.interactive-text').appendChild(newText);
+        
+        // Apply random animation
+        const animations = ['wave', 'bounce', 'fade', 'glitch', 'rainbow', 'shake', 'neon'];
+        const randomAnimation = animations[Math.floor(Math.random() * animations.length)];
+        applyAnimation(newText, randomAnimation);
+
+        // Add interaction handlers
+        newText.addEventListener('mouseenter', () => {
+            applyAnimation(newText, 'wave');
+        });
+
+        newText.addEventListener('mouseleave', () => {
+            newText.textContent = text;
+            newText.style.fontFamily = fontSelector.value;
+            newText.style.color = colorPicker.value;
+        });
+
+        newText.addEventListener('click', () => {
+            applyAnimation(newText, 'glitch');
+        });
+    }
+
     // Custom text animation
     animateBtn.addEventListener('click', () => {
         const text = customText.value.trim();
         if (text) {
-            // Create new animated text element
-            const newText = document.createElement('div');
-            newText.className = 'text-container';
-            newText.textContent = text;
-            newText.setAttribute('data-original-text', text);
-            
-            // Add to interactive text section
-            document.querySelector('.interactive-text').appendChild(newText);
-            
-            // Apply random animation
-            const animations = ['wave', 'bounce', 'fade', 'glitch'];
-            const randomAnimation = animations[Math.floor(Math.random() * animations.length)];
-            applyAnimation(newText, randomAnimation);
-
-            // Add interaction handlers
-            newText.addEventListener('mouseenter', () => {
-                applyAnimation(newText, 'wave');
-            });
-
-            newText.addEventListener('mouseleave', () => {
-                newText.textContent = text;
-            });
-
-            newText.addEventListener('click', () => {
-                applyAnimation(newText, 'glitch');
-            });
-
-            // Clear input
+            createAnimatedText(text);
             customText.value = '';
         }
+    });
+
+    // Preset text buttons
+    presetBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const presetText = btn.getAttribute('data-text');
+            createAnimatedText(presetText);
+        });
     });
 
     // Allow Enter key to trigger animation
@@ -121,4 +166,7 @@ document.addEventListener('DOMContentLoaded', () => {
             animateBtn.click();
         }
     });
+
+    // Initialize animation speed
+    document.documentElement.style.setProperty('--animation-speed', '1s');
 });
