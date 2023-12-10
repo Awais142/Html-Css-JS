d9222893e624bde177f9b597be8fd60d3a2565f1
Awais142 <awaisakbar142@gmail.com>
2023-12-10 10:30:00 +0500

    Beautiful Hero completed

---
+++ Beautiful-Hero/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('mousemove', (e) => {
+    const shapes = document.querySelectorAll('.shape');
+    const x = e.clientX / window.innerWidth;
+    const y = e.clientY / window.innerHeight;
+
+    shapes.forEach(shape => {
+        // Create a subtle parallax effect
+        const speed = 20;
+        const xOffset = (x - 0.5) * speed;
+        const yOffset = (y - 0.5) * speed;
+        
+        shape.style.transform = `translate(${xOffset}px, ${yOffset}px)`;
+    });
+});
