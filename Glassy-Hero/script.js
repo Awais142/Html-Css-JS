d8bc9d8f79d12f062888914dac2f2dd300d5d84d
Awais142 <awaisakbar142@gmail.com>
2023-12-11 10:50:00 +0500

    Beautiful bubbles completed

--- Glassy-Hero/script.js
+++ Glassy-Hero/script.js
@@ -1,13 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const bubbles = document.querySelectorAll('.bubbles span');
-    
-    // Randomize initial positions and delays
-    bubbles.forEach(bubble => {
-        bubble.style.left = `${Math.random() * 100}%`;
-        bubble.style.animationDelay = `${Math.random() * 15}s`;
-        bubble.style.animationDuration = `${25 + Math.random() * 15}s`;
-    });
-
     // Add mouse parallax effect to glass card
     const glassCard = document.querySelector('.glass-card');
     document.addEventListener('mousemove', (e) => {
