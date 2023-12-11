9d153b1eb78ebf3dbe80089269e2976c1e97fcd5
Awais142 <awaisakbar142@gmail.com>
2023-12-11 10:50:00 +0500

    Beautiful glass js added

---
+++ Glassy-Hero/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const bubbles = document.querySelectorAll('.bubbles span');
+    
+    // Randomize initial positions
+    bubbles.forEach(bubble => {
+        bubble.style.left = `${Math.random() * 100}%`;
+        bubble.style.animationDelay = `${Math.random() * 2}s`;
+    });
+
+    // Add mouse parallax effect to glass card
+    const glassCard = document.querySelector('.glass-card');
+    document.addEventListener('mousemove', (e) => {
+        const x = e.clientX / window.innerWidth;
+        const y = e.clientY / window.innerHeight;
+        
+        const xOffset = (x - 0.5) * 30;
+        const yOffset = (y - 0.5) * 30;
+        
+        glassCard.style.transform = `translate(${xOffset}px, ${yOffset}px)`;
+    });
+});
